84190f6e51ab2e77c154852b8321aaaeb5a38f21
Jamie Titus <ctitus@stanford.edu>
2021-07-21 17:27:43 -0400

    rename from sst_core -> sst_common

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     author="Charles Titus",
     author_email="charles.titus@nist.gov",
     install_requires=["bluesky", "ophyd", "sst_base"],
-    name="sst_core",
+    name="sst_common",
     version="0.1.0",
     packages=find_packages()
 )
