efaa31f270dd347eca32f7d3c093c8eb85d77b7d
Charles Titus <ctitus@bnl.gov>
2021-09-17 16:58:24 -0400

    parametrized sample position test

--- sst_common/tests/test_sample_position.py
+++ sst_common/tests/test_sample_position.py
@@ -11,7 +11,7 @@ from sst_common.plans.samples import move_to_sample_edge, move_to_sample_center
 @pytest.fixture()
 def loaded_manipulator(fresh_manipulator):
     samples = [(1, 'sample1', (1, 1, 9, 7), 1),
-               (2, 'sample2', (1, 9, 9, 15), 1),
+               (2, 'sample2', (1, 9, 2, 10), 1),
                (3, 'sample3', (1, 17, 9, 23), 1),
                (4, 'sample4', (1, 1, 9, 7), 2),
                (5, 'sample5', (1, 9, 9, 15), 2),
@@ -32,6 +32,7 @@ def test_sample_edge_position(RE, loaded_manipulator):
     assert manipulator.sample_distance_to_beam() < 0
 
 
-def test_sample_center_position(RE, loaded_manipulator):
-    RE(move_to_sample_center(1))
-    assert np.isclose(manipulator.sample_distance_to_beam(), 0)
+@pytest.mark.parametrize("sampleid", [1, 2, 3, 4, 5, 6])
+def test_sample_center_position(RE, loaded_manipulator, sampleid):
+    RE(move_to_sample_center(sampleid))
+    assert manipulator.sample_distance_to_beam() < 0
