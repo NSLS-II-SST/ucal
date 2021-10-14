dbd959d09cc8be1ef11836a6ed073aba641b1fca
SST Operator <xf07id1@xf07id-ws5.nsls2.bnl.local>
2021-10-14 18:50:27 -0400

    Working on startup script for ucal

--- sst_common/startup.py
+++ sst_common/startup.py
@@ -1,6 +1,8 @@
 # startup sequence for beamline
-import .motors
-import .mirrors
-import .valves
-import .shutters
-import .detectors
+import sst_common.motors as ucal_motors
+import sst_common.mirrors as ucal_mirrors
+import sst_common.shutters as ucal_shutters
+import sst_common.valves as ucal_valves
+from sst_common.shutters import psh10, psh7
+from sst_common.mirrors import mir3, mir1
+from sst_common.detectors import i400
--- sst_common/valves.py
+++ sst_common/valves.py
@@ -1,4 +1,4 @@
 from . import STATION_NAME
 
 if STATION_NAME == "ucal":
-    from sst_hw.valves import *
+    from sst_hw.gatevalves import *
