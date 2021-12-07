5b84aace61df0ba3c9a7450215754788d359cd63
SST Operator <xf07id1@xf07id-ws5.nsls2.bnl.local>
2021-12-07 15:12:16 -0500

    detector aliases added to startup

--- ucal_common/startup.py
+++ ucal_common/startup.py
@@ -7,7 +7,7 @@ import ucal_common.valves as ucal_valves
 #convenience imports
 from ucal_common.shutters import psh10, psh7
 from ucal_common.mirrors import mir1, mir3, mir4
-from ucal_common.detectors import ucal_i400, m5c_i400, dm7_i400
+from ucal_common.detectors import ucal_i400, m5c_i400, dm7_i400, tes, i0, sc
 from ucal_common.motors import manipx, manipy, manipz, manipr, tesz
 from ucal_hw.energy import en
 
