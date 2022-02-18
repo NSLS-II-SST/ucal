9a4a4588354da93a2c5b0d280d243a90b594a5af
Charles Titus <ctitus@bnl.gov>
2022-02-18 16:57:33 -0500

    shortened names of sample object signals

--- ucal_common/sampleholder.py
+++ ucal_common/sampleholder.py
@@ -2,7 +2,9 @@ from sst_base.sampleholder import SampleHolder
 from ucal_common.motors import manipulator
 
 sampleholder = SampleHolder(manipulator=manipulator, name="ucal_sampleholder")
-
+sampleholder.sample.sample_name.name = "sample_name"
+sampleholder.sample.sample_id.name = "sample_id"
+sampleholder.sample.sample_desc.name = "sample_desc"
 """
 geometry = make_two_sided_bar(10, 60, 1)
 refholder.add_geometry(geometry)
