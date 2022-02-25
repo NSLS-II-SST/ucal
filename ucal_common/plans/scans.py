2ae599da41b3263a61c6292b1a338b234fe6bd42
Charles Titus <ctitus@bnl.gov>
2022-02-25 10:29:49 -0500

    refactored plans

--- ucal_common/plans/scans.py
+++ ucal_common/plans/scans.py
@@ -1,26 +1,55 @@
-import numpy as np
-from bluesky.plans import list_scan
-from ucal_common.detectors import base_dets
+from .scan_base import wrap_xas, tes_gscan
+from ucal_hw.energy import en
 
-def make_gscan_list(e1, *args):
-    if len(args) < 2:
-        raise TypeError("make_gscan_list() requires at least estart, estop, and delta")
-    if len(args)%2 == 1:
-        raise TypeError("make_gscan_list() received an even number of arguments. Either a step or a step-size is missing")
-    estart = e1
-    positions = []
-    for estop, delta in zip(args[::2], args[1::2]):
-        n = 0
-        while n*delta + estart < estop - delta/10.0:
-            positions.append(n*delta + estart)
-            n += 1
-        estart = estop
-    positions.append(estop)
-    return positions
 
-def gscan_base(dets, motor, estart, estop, delta, *args):
-    position_list = make_gscan_list(estart, estop, delta, *args)
-    yield from list_scan(dets, motor, position_list)
+@wrap_xas("Zn")
+def tes_zn_xas(**kwargs):
+    yield from tes_gscan(en.energy, 1010, 1015, 0.5, 1030, 0.125, 1040, 0.2,
+                         1060, 0.25, 1070, 0.5, **kwargs)
 
-def gscan(motor, estart, estop, delta, *args):
-    yield from gscan_base(base_dets, motor, estart, estop, delta, *args)
+
+@wrap_xas("Cu")
+def tes_cu_xas(**kwargs):
+    yield from tes_gscan(en.energy, 915, 920, 0.5, 926, 0.25, 934.5, 0.17, 947,
+                         0.25, 954, 0.2, 970, 1, **kwargs)
+
+
+@wrap_xas("Ni")
+def tes_ni_xas(**kwargs):
+    yield from tes_gscan(en.energy, 840, 845, 0.5, 848, 0.2, 858, 0.1, 867,
+                         0.2, 874, 0.1, 882, 0.5, 890, 1, **kwargs)
+
+
+@wrap_xas("Co")
+def tes_co_xas(**kwargs):
+    yield from tes_gscan(en.energy, 765, 772, 0.4, 775, 0.2, 784, 0.1, 790,
+                         0.2, 797, 0.1, 800, 0.2, 805, 0.4, 810, 1, **kwargs)
+
+
+@wrap_xas("Fe")
+def tes_fe_xas(**kwargs):
+    yield from tes_gscan(en.energy, 690, 700, 1, 704, 0.5, 707, 0.2, 715, 0.1,
+                         719, 0.5, 725, 0.1, 730, 0.5, 760, 1, **kwargs)
+
+
+@wrap_xas("Mn")
+def tes_mn_xas(**kwargs):
+    yield from tes_gscan(en.energy, 630, 635, 0.5, 683, 0.2, 657, 0.1, 670,
+                         0.5, **kwargs)
+
+
+@wrap_xas("O")
+def tes_o_xas(**kwargs):
+    yield from tes_gscan(en.energy, 515, 520, 0.5, 526, 0.2, 535, 0.05, 545,
+                         0.1, 555, 0.25, 585, 0.5, 600, 1, **kwargs)
+
+
+@wrap_xas("Fe")
+def tes_fe_xas_short(**kwargs):
+    yield from tes_gscan(en.energy, 700, 705, 1, 720, 0.5, 740, 1, **kwargs)
+
+
+@wrap_xas("C")
+def tes_c_xas(**kwargs):
+    yield from tes_gscan(en.energy, 270, 280, 1, 300, 0.1, 310, 0.2, 350, 1,
+                         **kwargs)
