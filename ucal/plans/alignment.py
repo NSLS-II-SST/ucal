dbb81b8383327b0ad09686ae10dd992f51933e50
Charles Titus <ctitus@bnl.gov>
2022-06-27 17:21:11 -0400

    original find_side_basis replaced with efficient_find_side_basis, TES position check added to find_beam_x_offset

--- ucal/plans/alignment.py
+++ ucal/plans/alignment.py
@@ -1,6 +1,6 @@
 from bluesky.plan_stubs import mv, mvr
 # from bluesky.utils import Msg
-from ucal.motors import manipx, manipz, manipr, manipulator
+from ucal.motors import manipx, manipz, manipr, manipulator, tesz
 from ucal.plans.find_edges import (scan_r_coarse, scan_r_medium,
                                           scan_x_coarse, scan_x_medium, scan_x_fine,
                                           find_x_offset, find_z_offset, find_x_adaptive,
@@ -9,6 +9,8 @@ from ucal.plans.samples import set_side, sample_move
 from ucal.plans.plan_stubs import update_manipulator_side
 from ucal.configuration import beamline_config
 from sst_funcs.geometry.linalg import deg_to_rad, rad_to_deg, rotz, vec
+from bluesky.plan_stubs import rd
+from bluesky.utils import RequestAbort
 import numpy as np
 
 # need to fix imports, test, etc, actually hook up the max logic, and do a
@@ -17,6 +19,10 @@ import numpy as np
 
 
 def find_beam_x_offset():
+    tes_pos = yield from rd(tesz)
+    if tes_pos < 40:
+        print("TES too close, move back to 40 mm to continue")
+        raise RequestAbort
     yield from set_side(1)
     # We want to move off the sample
     yield from sample_move(-1, 5, 45)
@@ -73,15 +79,15 @@ def calculate_corner_y(x1, r1, x2, r2, nsides=4):
 
 def find_corner_known_rotation(r1, r2, nsides=4):
     yield from mv(manipr, r1)
-    x1 = yield from find_x_offset(precision=0.1)
+    x1 = yield from find_x()
     yield from mv(manipr, r2)
-    x2 = yield from find_x_offset(precision=0.1)
+    x2 = yield from find_x()
     y1 = calculate_corner_y(x1, r1, x2, r2, nsides)
     print(f"Corners known rotation x1: {x1}, y1: {y1}")
     return x1, y1
 
 
-def efficient_find_side_basis(nsides=4, x1=None, r1=None, x3=None, z=None, find_angle=False):
+def find_side_basis(nsides=4, x1=None, r1=None, x3=None, z=None, find_angle=False):
     """
     Efficiently finds the side basis and leaves manipulator ready for
     the next side
@@ -157,34 +163,6 @@ def find_side_points(x1, y1, x3, y3, z1, z3, r1, origin=0):
     return p1r, p2r, p3r
 
 
-def find_side_basis(nsides=4):
-    z = yield from find_z_offset()
-    yield from mvr(manipz, 5)
-    x1, y1, r1, r2 = yield from find_corner_coordinates(nsides)
-    yield from mvr(manipz, 90)
-    (x3, y3) = yield from find_corner_known_rotation(r1, r2, nsides)
-    yield from mv(manipz, z)
-    # fudging origin a bit so that it is z, not z + 5, could fix this
-    # by proper scaling along p1-p2 vector
-    origin = manipulator.origin[:3]
-    p1 = origin + vec(x1, y1, -z)
-    p2 = origin + vec(x3, y3, -(z + 90))
-    p3 = origin + vec(x1, y1 - 1, -z)
-    """
-    p1 = manipulator.manip_to_beam_frame(*vec(x1, y1, z), 0)[:3]
-    # still want correct height difference between points
-    p2 = manipulator.manip_to_beam_frame(*vec(x3, y3, z + 90), 0)[:3]
-    p3 = manipulator.manip_to_beam_frame(*vec(x1, y1 + 10, z), 0)[:3]
-    """
-    theta1 = deg_to_rad(r1)
-    p1r = rotz(theta1, p1)
-    p2r = rotz(theta1, p2)
-    p3r = rotz(theta1, p3)
-    print("Points ", p1, p2, p3)
-    print("Rotated points ", p1r, p2r, p3r)
-    return p1r, p2r, p3r
-
-
 def calibrate_side(side_num, nsides=4):
     p1, p2, p3 = yield from find_side_basis(nsides)
     yield from update_manipulator_side(side_num, p1, p2, p3)
@@ -198,8 +176,8 @@ def calibrate_sides(side_start, side_end, nsides=4):
     for side in range(side_start, side_end + 1):
         yield from set_side(side)
         yield from sample_move(0, 0, 0)
-        points, previous_side = yield from efficient_find_side_basis(nsides,
-                                                                     **previous_side, z=z)
+        points, previous_side = yield from find_side_basis(nsides,
+                                                           **previous_side, z=z)
         yield from update_manipulator_side(side, *points)
 
 
