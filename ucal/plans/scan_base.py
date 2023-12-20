9b7be8e696a81352668c4c88137a9d6e366c907f
Charles Titus <ctitus@bnl.gov>
2023-12-19 21:54:08 -0500

    added beamline setup decorator so that plans can move samples and eslit

--- ucal/plans/scan_base.py
+++ ucal/plans/scan_base.py
@@ -1,12 +1,22 @@
-from ucal.detectors import (tes, GLOBAL_ACTIVE_DETECTORS,
-                            GLOBAL_PLOT_DETECTORS,
-                            activate_detector, deactivate_detector)
+from ucal.detectors import (
+    tes,
+    GLOBAL_ACTIVE_DETECTORS,
+    GLOBAL_PLOT_DETECTORS,
+    activate_detector,
+    deactivate_detector,
+)
 from ucal.shutters import psh10
 from ucal.energy import en
-from ucal.plans.plan_stubs import call_obj, set_exposure, close_shutter, open_shutter, is_shutter_open
+from ucal.plans.plan_stubs import (
+    call_obj,
+    set_exposure,
+    close_shutter,
+    open_shutter,
+    is_shutter_open,
+)
 from ucal.scan_exfiltrator import ScanExfiltrator
 from ucal.sampleholder import sampleholder
-from ucal.motors import manipulator
+from ucal.motors import eslit as energy_slit
 from ucal.plans.samples import sample_move
 from ucal.multimesh import set_edge, refholder
 from ucal.configuration import beamline_config
@@ -20,6 +30,18 @@ import bluesky.plans as bp
 from functools import wraps
 
 
+def beamline_setup(func):
+    @wraps(func)
+    def inner(*args, sample=None, eslit=None, dwell=None, **kwargs):
+        if sample is not None:
+            yield from sample_move(0, 0, 45, sample)
+        if eslit is not None:
+            yield from mv(energy_slit, eslit)
+        return (yield from func(*args, exposure_time_s=dwell, **kwargs))
+
+    return inner
+
+
 def wrap_xas_setup(element):
     def decorator(func):
         @wraps(func)
@@ -27,13 +49,20 @@ def wrap_xas_setup(element):
             if auto_setup_xas:
                 yield from set_edge(element)
             return (yield from func(*args, **kwargs))
+
         return inner
+
     return decorator
 
 
 def wrap_xas(element):
     def decorator(func):
-        return add_to_scan_list(wrap_metadata({"edge": element, "scantype": "xas"})(wrap_xas_setup(element)(func)))
+        return add_to_scan_list(
+            wrap_metadata({"edge": element, "scantype": "xas"})(
+                wrap_xas_setup(element)(func)
+            )
+        )
+
     return decorator
 
 
@@ -44,16 +73,24 @@ def get_detector_plot_hints():
     for detector in GLOBAL_PLOT_DETECTORS:
         if hasattr(detector, "plot_hints"):
             h = detector.plot_hints()
-            plot_y_md += h.get('y', [])
-            plot_mca_md += h.get('mca', [])
-            style_hints.update(h.get('style_hints', {}))
+            plot_y_md += h.get("y", [])
+            plot_mca_md += h.get("mca", [])
+            style_hints.update(h.get("style_hints", {}))
         else:
-            plot_y_md += detector.hints.get('fields', [])
+            plot_y_md += detector.hints.get("fields", [])
     return plot_y_md, plot_mca_md, style_hints
 
 
 @add_to_scan_list
-def tes_count(*args, extra_dets=[], exposure_time_s=None, md=None, plot_hints=None, **kwargs):
+@beamline_setup
+def tes_count(
+    *args,
+    extra_dets=[],
+    exposure_time_s=None,
+    md=None,
+    plot_hints=None,
+    **kwargs,
+):
     """Count for a specified number of points
 
     Modifies count to automatically fill
@@ -61,7 +98,8 @@ def tes_count(*args, extra_dets=[], exposure_time_s=None, md=None, plot_hints=No
     Other detectors may be added on the fly via extra_dets
     ---------------------------------------------------------
     """
-    class dummymotor():
+
+    class dummymotor:
         name = "time"
         egu = "index"
 
@@ -75,22 +113,23 @@ def tes_count(*args, extra_dets=[], exposure_time_s=None, md=None, plot_hints=No
 
     md = md or {}
     plot_hints = plot_hints or {}
-    _md = {"sample_args": sampleholder.sample.read(),
-           "ref_args": refholder.sample.read(),
-           "sample_md": sampleholder.current_sample_md()}
-    if 'last_cal' in beamline_config:
-        _md['last_cal'] = beamline_config['last_cal']
-    if 'last_noise' in beamline_config:
-        _md['last_noise'] = beamline_config['last_noise']
+    _md = {
+        "sample_args": sampleholder.sample.read(),
+        "ref_args": refholder.sample.read(),
+        "sample_md": sampleholder.current_sample_md(),
+    }
+    if "last_cal" in beamline_config:
+        _md["last_cal"] = beamline_config["last_cal"]
+    if "last_noise" in beamline_config:
+        _md["last_noise"] = beamline_config["last_noise"]
     yhints, mcahints, stylehints = get_detector_plot_hints()
-    _plot_hints = {'x': ['time'], 'y': yhints, 'mca': mcahints, 'style': stylehints}
+    _plot_hints = {"x": ["time"], "y": yhints, "mca": mcahints, "style": stylehints}
     _plot_hints.update(plot_hints)
-    _md['plot_hints'] = _plot_hints
+    _md["plot_hints"] = _plot_hints
 
     _md.update(md)
 
-    ret = (yield from bp.count(GLOBAL_ACTIVE_DETECTORS, *args, md=_md,
-                               **kwargs))
+    ret = yield from bp.count(GLOBAL_ACTIVE_DETECTORS, *args, md=_md, **kwargs)
 
     for det in extra_dets:
         deactivate_detector(det)
@@ -102,8 +141,17 @@ tes_count.__doc__ += bp.count.__doc__
 
 
 def _tes_plan_wrapper(plan_function, plan_name):
-
-    def _inner(motor, *args, extra_dets=[], exposure_time_s=None, md=None, plot_hints=None, **kwargs):
+    @beamline_setup
+    def _inner(
+        motor,
+        *args,
+        extra_dets=[],
+        sample=None,
+        exposure_time_s=None,
+        md=None,
+        plot_hints=None,
+        **kwargs,
+    ):
         scanex = ScanExfiltrator(motor, en.energy.position)
         tes.scanexfiltrator = scanex
 
@@ -121,26 +169,34 @@ def _tes_plan_wrapper(plan_function, plan_name):
                "sample_side": sampleholder.sample.side.get(),
                "sample_origin": sampleholder.sample.origin.get()}
         """
-        _md = {"sample_args": sampleholder.sample.read(),
-               "ref_args": refholder.sample.read(),
-               "sample_md": sampleholder.current_sample_md()}
-        if 'last_cal' in beamline_config:
-            _md['last_cal'] = beamline_config['last_cal']
-        if 'last_noise' in beamline_config:
-            _md['last_noise'] = beamline_config['last_noise']
+        _md = {
+            "sample_args": sampleholder.sample.read(),
+            "ref_args": refholder.sample.read(),
+            "sample_md": sampleholder.current_sample_md(),
+        }
+        if "last_cal" in beamline_config:
+            _md["last_cal"] = beamline_config["last_cal"]
+        if "last_noise" in beamline_config:
+            _md["last_noise"] = beamline_config["last_noise"]
         yhints, mcahints, stylehints = get_detector_plot_hints()
-        _plot_hints = {'x': motor.hints.get('fields', []),
-                       'y': yhints, 'mca': mcahints, 'style': stylehints}
+        _plot_hints = {
+            "x": motor.hints.get("fields", []),
+            "y": yhints,
+            "mca": mcahints,
+            "style": stylehints,
+        }
         _plot_hints.update(plot_hints)
-        _md['plot_hints'] = _plot_hints
+        _md["plot_hints"] = _plot_hints
         _md.update(md)
-        ret = (yield from plan_function(GLOBAL_ACTIVE_DETECTORS, motor,
-                                        *args, md=_md, **kwargs))
+        ret = yield from plan_function(
+            GLOBAL_ACTIVE_DETECTORS, motor, *args, md=_md, **kwargs
+        )
 
         for det in extra_dets:
             deactivate_detector(det)
 
         return ret
+
     d = f"""Modifies {plan_function.__name__} to automatically fill
 dets with the TES detector and basic beamline detectors.
 Other detectors may be added on the fly via extra_dets
@@ -156,6 +212,7 @@ tes_scan = add_to_scan_list(_tes_plan_wrapper(bp.scan, "tes_scan"))
 tes_rel_scan = add_to_scan_list(_tes_plan_wrapper(bp.rel_scan, "tes_rel_scan"))
 tes_list_scan = add_to_scan_list(_tes_plan_wrapper(bp.list_scan, "tes_list_scan"))
 
+
 @add_to_scan_list
 def take_dark_counts():
     """Take dark counts to zero current-amplifiers"""
@@ -170,7 +227,7 @@ def take_dark_counts():
         # Clear offsets first
         for det in GLOBAL_ACTIVE_DETECTORS:
             detname = det.name
-            if hasattr(det, 'offset'):
+            if hasattr(det, "offset"):
                 det.offset.set(0)
 
         yield from bp.count(GLOBAL_ACTIVE_DETECTORS, 10, md={"scantype": "darkcounts"})
@@ -178,42 +235,48 @@ def take_dark_counts():
         table = run.primary.read()
         for det in GLOBAL_ACTIVE_DETECTORS:
             detname = det.name
-            if hasattr(det, 'offset'):
+            if hasattr(det, "offset"):
                 dark_value = float(table[detname].mean().values)
                 det.offset.set(dark_value)
         if shutter_open:
             yield from open_shutter()
+
     return (yield from inner())
+
 
 def tes_take_noise():
     """Close the shutter and take TES noise. Run after cryostat cycle"""
+
     @run_decorator(md={"scantype": "noise"})
     def inner_noise():
-        beamline_config['last_cal'] = None
-        beamline_config['last_noise'] = None
+        beamline_config["last_cal"] = None
+        beamline_config["last_noise"] = None
         shutter_open = yield from is_shutter_open()
         if shutter_open:
             yield from close_shutter()
         yield from call_obj(tes, "take_noise")
         if shutter_open:
             yield from open_shutter()
-    uid = yield from inner_noise()
-    beamline_config['last_noise'] = uid
-    return uid
 
+    uid = yield from inner_noise()
+    beamline_config["last_noise"] = uid
+    return uid
 
 
 def tes_take_projectors():
     """Take projector data for TES. Run with pulses from cal sample"""
+
     @run_decorator(md={"scantype": "projectors"})
     def inner_projectors():
-        #tes.rpc.file_start(tes.path, write_ljh=True, write_off=False,
+        # tes.rpc.file_start(tes.path, write_ljh=True, write_off=False,
         #                   setFilenamePattern=tes.setFilenamePattern)
-        #yield from sleep(30)
-        #tes._file_end()
+        # yield from sleep(30)
+        # tes._file_end()
         yield from open_shutter()
         yield from call_obj(tes, "take_projectors", time=30)
+
     return (yield from inner_projectors())
+
 
 @add_to_plan_list
 def tes_setup(should_take_dark_counts=True):
@@ -224,9 +287,9 @@ def tes_setup(should_take_dark_counts=True):
     """
     if should_take_dark_counts:
         yield from close_shutter()
-        deactivate_detector('tes')
+        deactivate_detector("tes")
         yield from take_dark_counts()
-    activate_detector('tes', plot=True)
+    activate_detector("tes", plot=True)
     yield from tes_take_noise()
     yield from tes_take_projectors()
     yield from call_obj(tes, "make_projectors")
@@ -235,14 +298,18 @@ def tes_setup(should_take_dark_counts=True):
 
 def _make_gscan_points(*args, shift=0):
     if len(args) < 3:
-        raise TypeError(f"gscan requires at least estart, estop, and delta, received {args}")
+        raise TypeError(
+            f"gscan requires at least estart, estop, and delta, received {args}"
+        )
     if len(args) % 2 == 0:
-        raise TypeError("gscan received an even number of arguments. Either a step or a step-size is missing")
+        raise TypeError(
+            "gscan received an even number of arguments. Either a step or a step-size is missing"
+        )
     start = float(args[0])
     points = [start + shift]
     for stop, step in zip(args[1::2], args[2::2]):
         nextpoint = points[-1] + step
-        while nextpoint < stop - step/2.0 + shift:
+        while nextpoint < stop - step / 2.0 + shift:
             points.append(nextpoint)
             nextpoint += step
         points.append(stop + shift)
@@ -251,7 +318,7 @@ def _make_gscan_points(*args, shift=0):
 
 @add_to_scan_list
 def tes_gscan(motor, *args, extra_dets=[], shift=0, **kwargs):
-    """A variable step scan of a motor, the TES detector, and the basic beamline detectors. 
+    """A variable step scan of a motor, the TES detector, and the basic beamline detectors.
 
     Other detectors may be added via extra_dets
 
@@ -269,7 +336,9 @@ def tes_gscan(motor, *args, extra_dets=[], shift=0, **kwargs):
 def tes_calibrate(time, sampleid, exposure_time_s=10, energy=980, md=None):
     """Take energy calibration for TES. Moves to specified sample"""
     yield from sample_move(0, 0, 45, sampleid)
-    return (yield from tes_calibrate_inplace(time, exposure_time_s, energy=energy, md=md))
+    return (
+        yield from tes_calibrate_inplace(time, exposure_time_s, energy=energy, md=md)
+    )
 
 
 @add_to_scan_list
@@ -282,10 +351,11 @@ def tes_calibrate_inplace(time, exposure_time_s=10, energy=980, md=None):
     md = md or {}
     _md = {"scantype": "calibration", "calibration_energy": energy}
     _md.update(md)
-    cal_uid = yield from tes_count(int(time//exposure_time_s),
-                                   exposure_time_s=exposure_time_s, md=_md)
+    cal_uid = yield from tes_count(
+        int(time // exposure_time_s), exposure_time_s=exposure_time_s, md=_md
+    )
     yield from mv(tes.cal_flag, False)
-    beamline_config['last_cal'] = cal_uid
+    beamline_config["last_cal"] = cal_uid
     yield from set_exposure(pre_cal_exposure)
     return cal_uid
 
@@ -303,6 +373,7 @@ def xas_factory(energy_grid, edge, name):
 
         """
         yield from tes_gscan(en.energy, *energy_grid, **kwargs)
+
     d = f"Perform an in-place xas scan for {edge} with energy pattern {energy_grid} \n"
     inner.__doc__ = d + inner.__doc__
 
