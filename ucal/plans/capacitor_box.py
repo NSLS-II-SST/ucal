41073eb0a1cab418303ede210c5cd9b15dd3d7df
Charles Titus <ctitus@bnl.gov>
2025-05-21 14:51:41 -0400

    Added capacitor box plan and startup

--- ucal/plans/capacitor_box.py
+++ ucal/plans/capacitor_box.py
@@ -1,10 +1,12 @@
 from nbs_bl.beamline import GLOBAL_BEAMLINE as bl
-from bluesky.plans import mv
 import bluesky.plan_stubs as bps
+from nbs_bl.help import add_to_plan_list
 
-
+@add_to_plan_list
 def charge_capacitor_box():
-    yield from mv(bl["capacitor_box"], 5)
-    yield from bps.sleep(60 * 5)
-    yield from mv(bl["capacitor_box"], 0)
+    """Charge PEY Capacitor box for 2 minutes, and then wait 3 minutes for settling
+    """
+    yield from bps.mv(bl["capacitor_box"], 5)
     yield from bps.sleep(60 * 2)
+    yield from bps.mv(bl["capacitor_box"], 0)
+    yield from bps.sleep(60 * 3)
--- ucal/startup.py
+++ ucal/startup.py
@@ -25,6 +25,7 @@ import nbs_bl.plans.scans
 from ucal.plans.tes_setup import *
 from ucal.plans.configuration import setup_ucal
 from ucal.plans.energy import tune_grating, change_grating
+from ucal.plans.capacitor_box import *
 from ucal.run_engine import RE
 
 # from ucal.configuration import beamline_config, new_proposal, load_saved_configuration
